f3a268d0aa0e196acee580fd46b54cf233c6d6ac
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-11-22 04:05:49 -0800

    select audio

--- app/src/main/java/org/visualize/App.java
+++ app/src/main/java/org/visualize/App.java
@@ -3,10 +3,12 @@ package org.visualize;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import be.tarsos.dsp.io.jvm.AudioPlayer;
 import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.GainProcessor;
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
 import be.tarsos.dsp.io.jvm.AudioDispatcherFactory;
@@ -16,27 +18,34 @@ import be.tarsos.dsp.pitch.PitchProcessor;
 import be.tarsos.dsp.pitch.PitchProcessor.PitchEstimationAlgorithm;
 import be.tarsos.dsp.util.fft.FFT;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class App extends Application implements PitchDetectionHandler {
 
+    private Thread audioThread;
     private AudioDispatcher dispatcher;
     private PitchEstimationAlgorithm algo = PitchEstimationAlgorithm.YIN;
+    GainProcessor gainProcessor = new GainProcessor(1.0);
     private double pitch;
     private float sampleRate = 44100;
     private int bufferSize = 2048;
     private int overlap = 1024;
-    private String fileName = "24003__erdie__explosion-mega-thunder.wav";
+    private String fileName = "src/main/resources/24003__erdie__explosion-mega-thunder.wav";
     private int numBins = 128;
-    private boolean logarithmic = true;
+    // private boolean logarithmic = true;
     private int minFrequency = 50;
     private int maxFrequency = 11000;
-    private static final int WIDTH = 800; // Window width
-    private static final int HEIGHT = 400;
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 500;
     private Rectangle[] bars = new Rectangle[numBins];
     private double[] amplitudeBins = new double[numBins];
 
@@ -44,9 +53,8 @@ public class App extends Application implements PitchDetectionHandler {
         if (dispatcher != null) {
             dispatcher.stop();
         }
-
         try {
-            File audioFile = new File(fileName);
+            File audioFile = new File("src/main/resources/" + fileName);
             dispatcher = AudioDispatcherFactory.fromFile(audioFile, bufferSize, overlap);
             AudioFormat format = AudioSystem.getAudioFileFormat(audioFile).getFormat();
             dispatcher.addAudioProcessor(new AudioPlayer(format));
@@ -54,6 +62,7 @@ public class App extends Application implements PitchDetectionHandler {
             e.printStackTrace();
         }
 
+        dispatcher.addAudioProcessor(gainProcessor);
         dispatcher.addAudioProcessor(new PitchProcessor(algo, sampleRate, bufferSize, this));
         dispatcher.addAudioProcessor(fftProcessor);
 
@@ -72,29 +81,24 @@ public class App extends Application implements PitchDetectionHandler {
 
             fft.forwardTransform(transformBuffer);
             fft.modulus(transformBuffer, amplitudes);
-            // int[] bins = binFrequencies(amplitudes, fft);
             binAmplitudes(amplitudes, transformBuffer);
 
-            // calculate amplitudes
             for (int i = 0; i < amplitudeBins.length; i++) {
                 float real = transformBuffer[2 * i];
                 float imaginary = transformBuffer[2 * i + 1];
                 amplitudeBins[i] = (float) Math.sqrt(real * real + imaginary * imaginary);
             }
 
-            // normalize magnitude
             double maxAmplitude = 0;
             for (double amplitude : amplitudeBins) {
                 maxAmplitude = Math.max(maxAmplitude, amplitude);
             }
 
-            // amplitude range is from 0 to 1
             for (int i = 0; i < amplitudeBins.length; i++) {
                 amplitudeBins[i] /= maxAmplitude;
-                // System.out.printf("amp: %f ", amplitudeBins[i]);
             }
-            updateBars();
 
+            updateBars();
             return true;
         }
 
@@ -110,16 +114,13 @@ public class App extends Application implements PitchDetectionHandler {
                 for (int j = startIndex; j < endIndex && j < amplitudes.length; j++) {
                     amplitudeSum += amplitudes[j];
                 }
-
                 amplitudeBins[i] = amplitudeSum / (endIndex - startIndex + 1);
             }
         }
 
         @Override
         public void processingFinished() {
-            // TODO Auto-generated method stub
         }
-
     };
 
     @Override
@@ -131,64 +132,49 @@ public class App extends Application implements PitchDetectionHandler {
         }
     }
 
-    public int[] binFrequencies(float[] amplitudes, FFT fft) {
-        List<Double> frequencies = extractFrequencies(amplitudes, fft);
-        int[] bins = new int[numBins];
-        for (double frequency : frequencies) {
-            int binIndex = frequencyToBin(frequency);
-            if (binIndex >= 0 && binIndex < numBins) {
-                bins[binIndex]++;
-            }
-        }
-        return bins;
-    }
-
-    private List<Double> extractFrequencies(float[] amplitudes, FFT fft) {
-        List<Double> frequencies = new ArrayList<>();
-
-        for (int binIndex = 0; binIndex < amplitudes.length; binIndex++) {
-            double frequency = binIndex * sampleRate / bufferSize;
-
-            if (amplitudes[binIndex] > 0.01) {
-                frequencies.add(frequency);
-            }
-        }
-        return frequencies;
-    }
-
-    private int frequencyToBin(double frequency) {
-        if (frequency < minFrequency || frequency > maxFrequency)
-            return -1;
-
-        if (logarithmic) {
-            double logMin = Math.log10(minFrequency);
-            double logMax = Math.log10(maxFrequency);
-            double logFreq = Math.log10(frequency);
-            return (int) ((logFreq - logMin) / (logMax - logMin) * numBins);
-        } else {
-            return (int) ((frequency - minFrequency) / (maxFrequency - minFrequency) * numBins);
-        }
-    }
-
-    public double[] getBinRange(int binIndex) {
-        if (logarithmic) {
-            double logMin = Math.log10(minFrequency);
-            double logMax = Math.log10(maxFrequency);
-            double logBinWidth = (logMax - logMin) / numBins;
-            double lowerBound = Math.pow(10, logMin + binIndex * logBinWidth);
-            double upperBound = Math.pow(10, logMin + (binIndex + 1) * logBinWidth);
-            return new double[] { lowerBound, upperBound };
-        } else {
-            double binWidth = (maxFrequency - minFrequency) / numBins;
-            double lowerBound = minFrequency + binIndex * binWidth;
-            double upperBound = minFrequency + (binIndex + 1) * binWidth;
-            return new double[] { lowerBound, upperBound };
-        }
-    }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane root = new Pane();
+        List<String> results = new ArrayList<String>();
+
+        File[] files = new File("src/main/resources").listFiles();
+
+        for (File file : files) {
+            if (file.isFile()) {
+                results.add(file.getName());
+            }
+        }
+
+        ListView<String> listView = new ListView<>();
+        listView.getItems().addAll(results);
+        listView.setStyle("-fx-background-color: black; -fx-text-fill: white;");
+        listView.setOnMouseClicked((MouseEvent event) -> {
+            String selectedFile = listView.getSelectionModel().getSelectedItem();
+            if (selectedFile != null) {
+                if (dispatcher != null) {
+                    dispatcher.stop();
+                }
+                if (audioThread != null && audioThread.isAlive()) {
+                    try {
+                        audioThread.join(); // Wait for the thread to finish
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                fileName = selectedFile;
+                setDispatch();
+            }
+        });
+
+        BorderPane root = new BorderPane();
+        root.setStyle("-fx-background-color: black;");
+
+        VBox menu = new VBox(10);
+        menu.setPadding(new Insets(10));
+        menu.setStyle("-fx-background-color: black;");
+        menu.getChildren().addAll(listView);
+
+        Pane visualizerPane = new Pane();
+        visualizerPane.setStyle("-fx-background-color: black;");
         double barWidth = (double) WIDTH / numBins;
 
         for (int i = 0; i < numBins; i++) {
@@ -199,27 +185,26 @@ public class App extends Application implements PitchDetectionHandler {
             bar.setY(HEIGHT);
             bar.setFill(Color.BLUE);
             bars[i] = bar;
-            root.getChildren().add(bar);
+            visualizerPane.getChildren().add(bar);
         }
+
+        root.setLeft(menu);
+        root.setCenter(visualizerPane);
 
         Scene scene = new Scene(root, WIDTH, HEIGHT, Color.BLACK);
         primaryStage.setTitle("Electric Sheep");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        new Thread(() -> {
-            setDispatch();
-        }).start();
     }
 
     private void updateBars() {
         javafx.application.Platform.runLater(() -> {
             for (int i = 0; i < amplitudeBins.length; i++) {
                 double amplitude = amplitudeBins[i];
-                double barHeight = amplitude * HEIGHT; // Scale amplitude to fit window height
+                double barHeight = amplitude * HEIGHT;
                 bars[i].setHeight(barHeight);
-                bars[i].setY(HEIGHT - barHeight); // Adjust y-position to align with bottom
-                bars[i].setFill(Color.color(amplitude, 0.5, 1.0)); // Change color based on amplitude
+                bars[i].setY(HEIGHT - barHeight);
+                bars[i].setFill(Color.color(amplitude, 0.5, 1.0));
             }
         });
     }
@@ -227,4 +212,68 @@ public class App extends Application implements PitchDetectionHandler {
     public static void main(String[] args) {
         launch(args);
     }
+
+    /*
+     * public int[] binFrequencies(float[] amplitudes, FFT fft) {
+     * List<Double> frequencies = extractFrequencies(amplitudes, fft);
+     * int[] bins = new int[numBins];
+     * for (double frequency : frequencies) {
+     * int binIndex = frequencyToBin(frequency);
+     * if (binIndex >= 0 && binIndex < numBins) {
+     * bins[binIndex]++;
+     * }
+     * }
+     * return bins;
+     * }
+     */
+
+    /*
+     * private List<Double> extractFrequencies(float[] amplitudes, FFT fft) {
+     * List<Double> frequencies = new ArrayList<>();
+     * 
+     * for (int binIndex = 0; binIndex < amplitudes.length; binIndex++) {
+     * double frequency = binIndex * sampleRate / bufferSize;
+     * 
+     * if (amplitudes[binIndex] > 0.01) {
+     * frequencies.add(frequency);
+     * }
+     * }
+     * return frequencies;
+     * }
+     */
+
+    /*
+     * private int frequencyToBin(double frequency) {
+     * if (frequency < minFrequency || frequency > maxFrequency)
+     * return -1;
+     * 
+     * if (logarithmic) {
+     * double logMin = Math.log10(minFrequency);
+     * double logMax = Math.log10(maxFrequency);
+     * double logFreq = Math.log10(frequency);
+     * return (int) ((logFreq - logMin) / (logMax - logMin) * numBins);
+     * } else {
+     * return (int) ((frequency - minFrequency) / (maxFrequency - minFrequency) *
+     * numBins);
+     * }
+     * }
+     */
+
+    /*
+     * public double[] getBinRange(int binIndex) {
+     * if (logarithmic) {
+     * double logMin = Math.log10(minFrequency);
+     * double logMax = Math.log10(maxFrequency);
+     * double logBinWidth = (logMax - logMin) / numBins;
+     * double lowerBound = Math.pow(10, logMin + binIndex * logBinWidth);
+     * double upperBound = Math.pow(10, logMin + (binIndex + 1) * logBinWidth);
+     * return new double[] { lowerBound, upperBound };
+     * } else {
+     * double binWidth = (maxFrequency - minFrequency) / numBins;
+     * double lowerBound = minFrequency + binIndex * binWidth;
+     * double upperBound = minFrequency + (binIndex + 1) * binWidth;
+     * return new double[] { lowerBound, upperBound };
+     * }
+     * }
+     */
 }
